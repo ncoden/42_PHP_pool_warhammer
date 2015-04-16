354ca02492d4832c68155124d06ba3db47bc3a9b
byoung-w <bwyyoung@gmail.com>
2015-04-16 15:18:50 +0200

    Empty game class added

---
+++ class/Game.class.php
@@ -0,0 +1,21 @@
+<?php
+
+class Game
+{
+	public static function		doc()
+	{
+		return (file_get_contents('Game.doc.txt'));
+	}
+
+	function __construct()
+	{
+
+	}
+
+	function __destruct()
+	{
+
+	}
+}
+
+?>
