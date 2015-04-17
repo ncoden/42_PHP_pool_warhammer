08c463271cf2d8c85caf7590c966e9698d963fbc
Nicolas CODEN <nicos.coden@neuf.fr>
2015-04-17 15:41:33 +0200

    Add DataBase::select, update, insert, req

--- class/DataBase.class.php
+++ class/DataBase.class.php
@@ -48,6 +48,78 @@ class DataBase
 			return (FALSE);
 	}
 
+	public static function		select($table, $id, $values)
+	{
+		if (is_array($args1)
+		&& $db = DataBase::connect())
+		{
+			// build the req
+			$req = 'SELECT ';
+
+			if (is_array($values))
+				$req .= implode($values, ', ');
+			else
+				$req .= '*';
+			$req .= ' FROM '.$table.' WHERE id = ?';
+
+			// request !
+			return (DataBase::req($sql, array($id)));
+		}
+		return (FALSE);
+	}
+
+	public static function		update($table, $id, $values)
+	{
+		if (is_array($value)
+		&& $db = DataBase::connect())
+		{
+			// build the req
+			$sql = 'UPDATE '.$table.' SET';
+			foreach ($values as $field => $value)
+				$sql .= $field.' = ?,';
+			$sql = substr($req, 0, -1);
+			$sql .= 'WHERE id = ?';
+
+			// add the id in the argument list
+			array_push($values, $id);
+
+			// request !
+			return (DataBase::req($sql, array_values($values)));
+		}
+		return (FALSE);
+	}
+
+	public static function		insert($table, $datas)
+	{
+		if (!empty($datas))
+		{
+			$sql = 'INSERT INTO '.$table.' ('.implode(array_keys($datas), ',').') VALUES (?';
+
+			$count = count($datas) - 1;
+			for ($i = 0; $i < $count; $i++)
+				$sql .= ',?';
+			$sql .= ')';
+
+			return (DataBase::req($sql, array_values($datas)));
+		}
+		return (FALSE);
+	}
+
+	public static function		req($sql, $datas = [])
+	{
+		if ($db = DataBase::connect())
+		{
+			$req = $db->prepare($req);
+			if ($db->execute(array_values($datas)))
+			{
+				$return = array();
+				while ($row = $req->fetch())
+					array_push($return, $row);
+				return ($return);
+			}
+		}
+		return (FALSE);
+	}
 }
 
 ?>
