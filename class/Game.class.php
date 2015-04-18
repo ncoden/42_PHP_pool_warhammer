47bc20d5da8a4b1d8abef5013021dc43469689fc
Sebastien CHONG <schong@e1r10p11.42.fr>
2015-04-18 16:38:26 +0200

    class change CheckEnd

--- class/Game.class.php
+++ class/Game.class.php
@@ -9,6 +9,9 @@ class Game
 	private						$_mapId;
 	private						$_bigTurn;
 	private						$_smallTurn;
+	const						GAME_EQUAL = 0;
+	const						INGAME = 1;
+	const						GAME_FINISHED = 2;
 
 	public static				create()
 	{
@@ -24,8 +27,8 @@ class Game
 		$lastIdMap = Database::getLastEntry('games'); // trouver dans la database le dernier element inserer
 		DataBase::insert('games', array(
 			'map_id' => $lastIdMap,
-			'winnerId' => 0,
-			'state' => 0,
+			'winnerId' => null,
+			'state' => 1,
 			'playerTurn' => 0,
 			'bigTurn' => 0,
 			'smallTurn' => 0
@@ -34,7 +37,7 @@ class Game
 
 	public function				__construct(array $kwargs)
 	{
-		if (isset($kwargs['gameId'])
+		if (isset($kwargs['id'])
 			&& isset($kwargs['winnerId'])
 			&& isset($kwargs['state'])
 			&& isset($kwargs['playerTurn'])
@@ -42,7 +45,7 @@ class Game
 			&& isset($kwargs['bigTurn'])
 			&& isset($kwargs['smallTurn']))
 		{
-			$this->_gameId = $kwargs['gameId'];
+			$this->_gameId = $kwargs['id'];
 			$this->_winnerId = $kwargs['winnerId'];
 			$this->_state = $kwargs['state'];
 			$this->_playerTurn = $kwargs['playerTurn'];
@@ -52,21 +55,46 @@ class Game
 		}
 	}
 
-
-/*
-	public function Victory()
+	public function				checkEnd($id)
 	{
-		$element = Licorne::getMap();
-		foreach ($element['Ships'] as $key => $value)
-		{
-			if ($value->_owner)
-			{
+		$allShip = InstanceManager::getAllShip($id);
+		var $fighter = 0;
+		var $check = 0;
+		var $id_winner = 0;
 
+		foreach ($allship as $key => $value)
+		{
+			if ($fighter == 0)
+			{
+				if ($value->_player != 0)
+					$fighter = $value->_player;
+			}
+			else
+			{
+				if ($fighter == $value->_player)
+				{
+					if ($value->state == Ship::STATE_OK)
+						$check++;
+					if ($check == 1)
+						$id_winner = $value->_player;
+				}
+				else
+				{
+					if ($value->state == Ship::STATE_OK)
+						$check++;
+					if ($check == 1)
+						$id_winner = $value->_player;
+				}
 			}
 		}
-		$ship->state = Ship::STATE_KILLED;
+
+		if ($check == 0)
+			return (array(GAME_EQUAL, 0));
+		else if ($check == 1)
+			return (array(GAME_FINISHED, $winner_id))
+		else
+			return (array(INGAME, 0));
 	}
-*/
 }
 
 ?>
